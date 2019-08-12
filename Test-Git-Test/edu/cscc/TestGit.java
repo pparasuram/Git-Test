9b20cc8b45048a4984e21859e908743a060d3916
Parasuram, Prakash (Prakash) <prakash@nationwide.com>
2019-08-12 12:37:27 -0400

    Initial commit.

---
+++ Test-Git-Test/edu/cscc/TestGit.java
@@ -0,0 +1,14 @@
+package edu.cscc;
+
+import org.junit.Test;
+
+import static edu.cscc.Main.bla_bla;
+import static org.junit.Assert.assertEquals;
+
+public class TestGit {
+    @Test
+    public void test (){
+        assertEquals ("hi", bla_bla());
+
+    }
+}
---
+++ src/edu/cscc/Main.java
@@ -0,0 +1,13 @@
+package edu.cscc;
+
+public class Main {
+
+    public static String bla_bla () {
+        return("hi");
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println(bla_bla());
+    }
+}
